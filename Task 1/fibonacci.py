a66a88e12c6eb6d5b5d6571871cdfa0cbadbf69f
SURIYAPRAKASH0212 <suriya22714@gmail.com>
2026-02-08 10:06:34 +0530

    Task 4 Programms Added

--- Task 1/fibonacci.py
+++ Task 1/fibonacci.py
@@ -2,7 +2,7 @@ number = int(input("Enter a number: "))
 first = 0
 last = 1
 for i in range(number):
-    print(first)
+    print(first,end=" ")
     next = first + last
     first = last
     last = next
